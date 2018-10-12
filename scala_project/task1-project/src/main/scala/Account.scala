964775fb432a98d39d0f4185cc7290a9d8e11d2a
Zawadi Berg Svela <z.b.svela@gmail.com>
2018-10-12 10:08:49 +0200

    Passed first 6 tests

--- scala_project/task1-project/src/main/scala/Account.scala
+++ scala_project/task1-project/src/main/scala/Account.scala
@@ -7,11 +7,25 @@ class Account(val bank: Bank, initialBalance: Double) {
     val balance = new Balance(initialBalance)
     val uid = bank.generateAccountId
 
-    def withdraw(amount: Double): Unit = ??? 
-    def deposit(amount: Double): Unit = ??? 
-    def getBalanceAmount: Double = ??? 
+    def withdraw(amount: Double): Unit = {
+        if(amount < 0) throw new exceptions.IllegalAmountException()
+        if(balance.amount < amount) throw new exceptions.NoSufficientFundsException()
+        balance.synchronized {
+            balance.amount -= amount
+        }
+    }
+    def deposit(amount: Double): Unit = {
+        if(amount < 0) throw new exceptions.IllegalAmountException()
+        balance.synchronized {
+            balance.amount += amount
+        }
+    }
+    def getBalanceAmount: Double = {
+        balance.amount
+    }
 
     def transferTo(account: Account, amount: Double) = {
+        if(amount < 0) throw new exceptions.IllegalAmountException()
         bank addTransactionToQueue (this, account, amount)
     }
 
--- scala_project/task1-project/src/main/scala/Bank.scala
+++ scala_project/task1-project/src/main/scala/Bank.scala
@@ -2,20 +2,29 @@ import scala.concurrent.forkjoin.ForkJoinPool
 
 class Bank(val allowedAttempts: Integer = 3) {
 
-    private val uid = ???
+    private val uid = 14
     private val transactionsQueue: TransactionQueue = new TransactionQueue()
     private val processedTransactions: TransactionQueue = new TransactionQueue()
-    private val executorContext = ???
+    private val executorContext = new ForkJoinPool(4)
 
     def addTransactionToQueue(from: Account, to: Account, amount: Double): Unit = {
       transactionsQueue push new Transaction(
         transactionsQueue, processedTransactions, from, to, amount, allowedAttempts)
     }
 
-    // Hint: use a counter 
-    def generateAccountId: Int = ???
+    // Hint: use a counter
+    private object Counter {
+        var prevId = 0
+    }
+    def generateAccountId: Int = {
+        Counter.synchronized{
+            Counter.prevId += 1
+            Counter.prevId
+        }
+    }
 
-    private def processTransactions: Unit = ???
+    private def processTransactions: Unit = {
+    }
 
     def addAccount(initialBalance: Double): Account = {
         new Account(this, initialBalance)
